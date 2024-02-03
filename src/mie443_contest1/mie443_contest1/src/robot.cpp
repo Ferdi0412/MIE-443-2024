465c31af7b2213b19b08aa72199bf804c41d87bf
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-02-03 15:25:30 -0500

    Added linear motion by distance

--- src/mie443_contest1/mie443_contest1/src/robot.cpp
+++ src/mie443_contest1/mie443_contest1/src/robot.cpp
@@ -1,9 +1,19 @@
+/**
+ * Provides Team1::Robot class
+ *
+ * Implements methods to control the kobuki robot base... See documentation at:
+ * http://wiki.ros.org/kobuki/Tutorials/Kobuki%27s%20Control%20System#How_it_works
+*/
+#ifndef ROBOT_V2_CPP
+#define ROBOT_V2_CPP
+
 #include <nav_msgs/Odometry.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <ros/console.h>
 #include "ros/ros.h"
 
 #include <exception>
+#include <math.h>
 // #include <stdio.h>
 
 #ifndef BUMPER_TOPIC
@@ -22,167 +32,258 @@
 #define VEL_COMMAND_TOPIC "cmd_vel_mux/input/teleop"
 #endif
 
-namespace MotionExceptions {
-
-    /**
-     * BumperException is thrown in certain motion methods, when a bumper is pressed in the direction of travel
-    */
-    class BumperException : public std::exception {};
-}
+/**
+ * BumperException is thrown when bumper is pressed when travelling.
+*/
+class BumperException : std::exception {};
 
 namespace Team1 {
     class Robot {
         private:
-            ros::Subscriber bumper_sub, laser_sub, odom_sub; // vel_sub?
-            ros::Publisher  vel_pub; // To move robot
+            /* === PRIVATE VARIABLES === */
+            ros::Subscriber bumper_sub, laser_sub, odom_sub, vel_sub;
+            ros::Publisher  vel_pub;
 
-            double _pos_x, _pos_y, _pos_theta;
-            double _vel_x, _vel_y, _vel_theta;
-            bool   _bumper_right, _bumper_left, _bumper_center;
+            double pos_x, pos_y, pos_theta;
+            double vel_x, vel_y, vel_theta;
 
-            void (*_spin_once)(void);
-            ros::Rate _read_rate;
+            double vel_fwd_act, vel_clock_act; // Internal state from robot
+            geometry_msgs::Twist motion_set;   // Set/command to send to robot
 
+            bool   bumper_right, bumper_left, bumper_center;
+
+            // void (*spin_once_ros)(void); // Try use ros::spinOnce(); instead of a callback
+            ros::Rate spin_rate;
+
+            /* === SUBSCRIPTION CALLBACKS === */
             /**
-             * odomCallback runs on the /odom topic subscription, updating the values in the robot object to the current state
+             * odomCallback runs per cycle on the /odom topic
              *
-             * @param msg the incoming message
+             * This topic displays the robot's position and velocity relative to it's "global" coordinates.
+             *
+             * @param msg the incoming Odometry message
             */
             void odomCallback( const nav_msgs::Odometry::ConstPtr& msg ) {
-                _pos_x =     msg->pose.pose.position.x;
-                _pos_y =     msg->pose.pose.position.y;
-                _pos_theta = msg->pose.pose.orientation.z;
+                pos_x     = msg->pose.pose.position.x;
+                pos_y     = msg->pose.pose.position.y;
+                pos_theta = msg->pose.pose.orientation.z;
 
-                _vel_x =     msg->twist.twist.linear.x;
-                _vel_y =     msg->twist.twist.linear.y;
-                _vel_theta = msg->twist.twist.angular.z;
+                vel_x     = msg->twist.twist.linear.x;
+                vel_y     = msg->twist.twist.linear.y;
+                vel_theta = msg->twist.twist.angular.z;
+            }
+
+            /**
+             * velocityCallback to be set on the /mobile_base/commands/velocity
+             *
+             * This topic displays what velocity the robot (should) be going with respect to it's base (ie. fwd/clockwise).
+             *
+             * @param msg Twist object from the topic
+            */
+            void velocityCallback( const geometry_msgs::Twist::ConstPtr& msg ) {
+                vel_fwd_act   = msg->linear.x;
+                vel_clock_act = msg->angular.z;
             }
 
             /**
              * bumperCallback runs on the /mobile_base/events/bumper topic, updating to reflect state of bumpers
+             * @param msg the incoming bumper message
             */
             void bumperCallback( const kobuki_msgs::BumperEvent::ConstPtr& msg ) {
                 switch (msg->bumper) {
                     case kobuki_msgs::BumperEvent::LEFT:
-                        _bumper_left = msg->state; break;
+                        bumper_left = msg->state; break;
 
                     case kobuki_msgs::BumperEvent::CENTER:
-                        _bumper_center = msg->state; break;
+                        bumper_center = msg->state; break;
 
                     case kobuki_msgs::BumperEvent::RIGHT:
-                        _bumper_right = msg->state; break;
+                        bumper_right = msg->state; break;
 
                     default:
-                        // OS_WARNING("[Team1Robot->bumperCallback] {msg->bumper} not recognized: %d := %d\n", msg->bumper, msg->state);
                         break;
                 }
             }
 
+            /* === AUXILLIARY FUNCTIONS === */
+            /**
+             * getEuclideanDistance computes the euclidean distance between two points (NOTE: no overflow checking)
+             *
+             * @param x1 x-coordinate of position 1
+             * @param y1 y-coordinate of position 1
+             * @param x2 x-coordinate of position 2
+             * @param y2 y-coordinate of position 2
+            */
+            double getEuclideanDistance( double x1, double y1, double x2, double y2 ) {
+                double dist = pow( x2 - x1, 2 ) + pow( y2 - y1, 2 );
+                return sqrt(dist);
+            }
+
         public:
+            /* === PUBLIC GETTERS === */
+            double getX()     { return pos_x; }
+            double getY()     { return pos_y; }
+            double getTheta() { return pos_theta; }
+
+            double getVelX()     { return vel_x; }
+            double getVelY()     { return vel_y; }
+            double getVelTheta() { return vel_theta; }
+
+            double getVelFwdAct()   { return vel_fwd_act; }
+            double getVelClockAct() { return vel_clock_act; }
+
+            double getVelFwdSet()   { return motion_set.linear.x; }
+            double getVelClockSet() { return motion_set.angular.z; }
+
+            bool getBumperLeft()   { return bumper_left; }
+            bool getBumperCenter() { return bumper_center; }
+            bool getBumperRight()  { return bumper_right; }
+            bool getBumperAny()    { return bumper_left || bumper_center || bumper_right; }
+
+            /* === ROS METHODS === */
             /**
-             * "getter" functions
+             * publishVelocity will publish the velocity control topic
             */
-            double getX() { return _pos_x; }
-            double getY() { return _pos_y; }
-            double getTheta() { return _pos_theta; }
-
-            double getVelX() { return _vel_x; }
-            double getVelY() { return _vel_y; }
-            double getVelTheta() { return _vel_theta; }
-
-            bool getBumperLeft() { return _bumper_left; }
-            bool getBumperCenter() { return _bumper_center; }
-            bool getBumperRight() { return _bumper_right; }
-            bool getBumperAny() { return _bumper_left || _bumper_center || _bumper_right; }
+            void publishVelocity( void ) {
+                vel_pub.publish( motion_set );
+            }
 
             /**
-             * spinOnceROS will update subscriptions and values in object...
+             * spinOnceROS will update class values with any topic changes/messages (and publish topics)
             */
-            void spinOnceROS( void ) { _spin_once(); }
+            void spinOnce( void ) {
+                publishVelocity();
+                ros::spinOnce();
+            }
 
             /**
-             * sleepROS will sleep for the rest of the read_interval
+             * sleepOnce will sleep for the rest of a "cycle" -> defined by the spin_rate
             */
-            void sleepROS( void ) { _read_rate.sleep(); }
+            void sleepOnce( void ) {
+                spin_rate.sleep();
+            }
+
+            /* === MOTION CONTROL === */
+            /**
+             * setMotion will set and publish the velocity components relative to the turtlebot base
+             *
+             * @param fwd_speed   is the linear speed (in m/s) to travel forwards
+             * @param clock_speed is the angular speed (in rad/s) to travel clockwise
+            */
+            void setMotion( double fwd_speed, double clock_speed ) {
+                motion_set.linear.x  = fwd_speed;
+                motion_set.angular.z = clock_speed;
+                publishVelocity();
+            }
 
             /**
-             * stopMotion will stop the last motion command published
+             * stopMotion will stop all motion of the robot
             */
             void stopMotion( void ) {
-                geometry_msgs::Twist stop_motion;
-                stop_motion.angular.z = 0;
-                stop_motion.linear.x  = 0;
-                vel_pub.publish(stop_motion);
-                spinOnceROS();
+                setMotion(0, 0);
             }
 
             /**
-             * jogForwards will start moving the robot forwards by a given speed for an indefinite distance
+             * jogForwards will start a forwards motion
              *
-             * @param velocity linear velocity [try find units...]
+             * @param velocity linear velocity [m/s]
             */
             void jogForwards( double velocity ) {
-                geometry_msgs::Twist fwd_motion;
-                fwd_motion.angular.z = 0;
-                fwd_motion.linear.x  = velocity;
-                vel_pub.publish(fwd_motion);
-                spinOnceROS();
+                setMotion( velocity, 0 );
             }
 
             /**
-             * jogForwardsSafe will first check if any of the bumpers are pressed
+             * jogForwardsSafe will start a forwards motion, and check if bumpers are triggered in the forwards direction
              *
-             * @param velocity linear velocity [try find units..]
-             *
-             * @throws MotionExceptions::BumperException when going to move forwards, but a bumper is triggered
+             * @param velocity linear velocity [m/s]
+             * @throws BumperException
             */
             void jogForwardsSafe( double velocity ) {
                 if ( (velocity > 0) && getBumperAny() )
-                    throw MotionExceptions::BumperException();
-                jogForwards( velocity );
+                    throw BumperException();
+                setMotion( velocity, 0 );
             }
 
             /**
-             * jogClockwise will start rotating the robot clockwise by a given speed for an indefinite angle of rotation
+             * moveForwards will move the robot forwards, and check if bumpers are triggered during the motion
+             * BLOCKING -> will not return until movement has completed
              *
-             * @param velocity angular velocity [try find units...]
+             * @param velocity linear velocity [m/s]         -> Negative to move backwards
+             * @param distance linear distance to travel [m] -> MUST ALWAYS BE POSITIVE
+             * @throws BumperException       -> If a bumper is triggered
+             * @throws std::invalid_argument -> If the distance is less than 0
+            */
+            void moveForwards( double velocity, double distance ) {
+                double start_x = pos_x, start_y = pos_y;
+                if ( distance < 0 ) throw std::invalid_argument;
+                jogForwardsSafe( velocity );
+                while ( getEuclideanDistance(start_x, start_y, pos_x, pos_y) < distance ) {
+                    checkBumpers(); // Stop and throw BumperException if bumpers triggered
+                    spinOnce();     // Update pos_x and pos_y
+                }
+            }
+
+            /**
+             * distanceToPoint will return the euclidean distance from the current position to a point (NOTE: No overflow handling)
+             *
+             * @param target_x is the target x-coordinate
+             * @param target_y is the target y-coordinate
+            */
+            double distanceToPoint( double target_x, double target_y ) {
+                return getEuclideanDistance( pos_x, pos_y, target_x, target_y );
+            }
+
+            /**
+             * jogClockwise will start a clockwise rotation, and check if bumpers in that direction are triggered
+             *
+             * @param velocity rotational velocity [rad/s]
             */
             void jogClockwise( double velocity ) {
-                geometry_msgs::Twist clock_motion;
-                clock_motion.angular.z = velocity;
-                clock_motion.linear.x  = 0;
-                vel_pub.publish(clock_motion);
-                spinOnceROS();
+                setMotion( 0, velocity );
             }
 
             /**
-             * jogClockwiseSafe will first check if the bumper in the direction of rotation is pressed
+             * jogClockwiseSafe will start a clockwise rotation, and check if bumpers in that direction are triggered
              *
-             * @param velocity angular velocity [try find units...]
-             *
-             * @throws MotionExceptions::BumperException when bumper on side of rotation is pressed
+             * @param velocity rotational velocity [rad/s]
+             * @throws BumperException
             */
             void jogClockwiseSafe( double velocity ) {
-                if ( (velocity > 0) && getBumperRight() )
-                    throw MotionExceptions::BumperException();
-                else if ( (velocity < 0) && getBumperLeft() )
-                    throw MotionExceptions::BumperException();
-                jogClockwise( velocity );
+                if ( getBumperCenter() )                  throw BumperException();
+                if ( (velocity > 0) && getBumperRight() ) throw BumperException();
+                if ( (velocity < 0) && getBumperLeft() )  throw BumperException();
+                // If no problems with bumpers...
+                setMotion( 0, velocity );
             }
 
             /**
-             * Robot constructor
+             * checkBumpers will read the bumper states, and stop motion if any are triggered
              *
-             * @param node_handler object used to configure subscriptions/publish topics for ROS
-             * @param read_interval a Rate object used to define read intervals in BLOCKING calls
-             * @param spin_once_function function that takes no parameters, used to update subscriptions in BLOCKING calls
+             * @throws BumperException
             */
-            Robot( ros::NodeHandle node_handler, ros::Rate& read_rate_object, void (*spin_once_function)(void) ) : _read_rate(read_rate_object) {
+            void checkBumpers( void ) {
+                if ( ((getVelFwdSet() > 0) && getBumperAny()) || ((getVelClockSet() > 0) && getBumperRight()) || ((getVelClockSet() < 0) && getBumperLeft()) ) {
+                    setMotion(0, 0);
+                    throw BumperException();
+                }
+            }
+
+            /* === CONSTRUCTORS/DESTRUCTORS === */
+            /**
+             * Robot class constructor
+             *
+             * @param node_handler   object used to configure subscriptions/publish topics for ROS
+             * @param spin_frequency the rate for use in BLOCKING calls (to be added...) [Hz]
+            */
+            Robot( ros::NodeHandle node_handler, double spin_frequency ) : spin_rate(spin_frequency) {
+                // Configure subscribers
                 bumper_sub = node_handler.subscribe(BUMPER_TOPIC, 10, &Team1::Robot::bumperCallback, this);
                 // laser_sub  = node_handler.subscribe(LASER_TOPIC, 1, &laserCallback);
                 odom_sub   = node_handler.subscribe(ODOM_TOPIC, 1, &Team1::Robot::odomCallback, this);
+                // Configure publisher
                 vel_pub    = node_handler.advertise<geometry_msgs::Twist>(VEL_COMMAND_TOPIC, 1);
-                _spin_once = spin_once_function;
-            };
+            }
     };
 }
+
+#endif
