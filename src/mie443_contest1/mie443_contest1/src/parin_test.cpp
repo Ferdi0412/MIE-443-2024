b85f8a311e4bde91ee5847950792c64bcc02640b
parinj05 <66892409+parinj05@users.noreply.github.com>
2024-02-06 21:40:17 -0500

    Update parin_test.cpp

--- src/mie443_contest1/mie443_contest1/src/parin_test.cpp
+++ src/mie443_contest1/mie443_contest1/src/parin_test.cpp
@@ -1,87 +1,89 @@
-#include <vector>
-#include <cmath>
-#include <chrono>
-#include <stdint.h>
-#include <stdio.h>
-
-#include <ros/console.h>
-#include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
-#include "sensor_msgs/LaserScan.h"
-
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/LaserScan.h>
 #include "robot.cpp"
 
-// Define constants
+// Constants
 #define LINEAR_SPEED 0.2
 #define ANGULAR_SPEED 0.5
-#define WALL_DISTANCE_THRESHOLD 1.0 // Adjust according to your environment
+#define WALL_DISTANCE_THRESHOLD 0.5 // Adjust according to your environment
 #define MAP_COVERAGE_THRESHOLD 0.8  // Adjust according to your requirement
 
 // Global variables
-static std::chrono::time_point<std::chrono::system_clock> program_start;
-static const unsigned long long program_duration = 10; // Adjust as needed
+static ros::Publisher velocity_publisher;
+static ros::Subscriber laser_subscriber;
+static double front_distance = 0.0;
+static bool obstacle_detected = false;
+static bool wall_following_mode = false;
 
 // Function declarations
-void moveAndScan_example(Team1::Robot robot, double distance);
-void wallFollowing(Team1::Robot &robot);
-void stopRobot(Team1::Robot &robot);
-uint16_t secondsElapsed(void);
-void exitIfTimeRunOut(void);
-void exitIfTimeRunOut(unsigned int exit_code);
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+void moveRobot(double linear_speed, double angular_speed);
+void stopRobot();
+void wallFollowing();
+bool isObstacleInFront();
 
 int main(int argc, char **argv) {
-    // Setup ROS node
-    ROS_INFO("Setting up...");
-
-    ros::init(argc, argv, "contest1");
+    // Initialize ROS node
+    ros::init(argc, argv, "wall_following");
     ros::NodeHandle nh;
-    ros::Rate loop_rate(2);
 
+    // Create robot object
     Team1::Robot robot(nh, 2);
-    ros::Duration(0.5).sleep(); // Ensure initialization
-    robot.spinOnce();
-    program_start = std::chrono::system_clock::now();
+
+    // Setup publishers and subscribers
+    velocity_publisher = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    laser_subscriber = nh.subscribe("/scan", 10, laserCallback);
 
     // Main loop
-    while (ros::ok() && secondsElapsed() <= program_duration) {
-        wallFollowing(robot);
+    while (ros::ok()) {
+        if (isObstacleInFront()) {
+            // If obstacle detected in front, start wall-following mode
+            wall_following_mode = true;
+        }
+
+        if (wall_following_mode) {
+            wallFollowing();
+        } else {
+            // Move forward
+            moveRobot(LINEAR_SPEED, 0.0);
+        }
+
         ros::spinOnce();
-        loop_rate.sleep();
     }
 
-    // Program end
-    stopRobot(robot);
-    ROS_INFO("Program ended.");
+    return 0;
 }
 
-void wallFollowing(Team1::Robot &robot) {
-    double front_distance = robot.getRanges()[robot.getNLasers() / 2]; // Front distance from laser scan
-
-    // Check if there's an obstacle in front
-    if (front_distance < WALL_DISTANCE_THRESHOLD) {
-        // Turn right to follow the wall
-        robot.setMotion(LINEAR_SPEED, -ANGULAR_SPEED);
-    } else {
-        // Move forward while following the wall
-        robot.setMotion(LINEAR_SPEED, 0);
-    }
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+    // Update front distance from laser scan
+    front_distance = msg->ranges[msg->ranges.size() / 2];
 }
 
-void stopRobot(Team1::Robot &robot) {
+void moveRobot(double linear_speed, double angular_speed) {
+    // Publish velocity command to move the robot
+    geometry_msgs::Twist vel_msg;
+    vel_msg.linear.x = linear_speed;
+    vel_msg.angular.z = angular_speed;
+    velocity_publisher.publish(vel_msg);
+}
+
+void stopRobot() {
     // Stop the robot's motion
-    robot.stopMotion();
+    moveRobot(0.0, 0.0);
 }
 
-uint16_t secondsElapsed(void) {
-    return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - program_start).count();
+void wallFollowing() {
+    if (front_distance > WALL_DISTANCE_THRESHOLD) {
+        // If no obstacle in front, move forward while following the wall
+        moveRobot(LINEAR_SPEED, -ANGULAR_SPEED); // Turn right
+    } else {
+        // If obstacle in front, turn left to avoid it
+        moveRobot(LINEAR_SPEED, ANGULAR_SPEED); // Turn left
+    }
 }
 
-void exitIfTimeRunOut(void) {
-    if (secondsElapsed() > program_duration)
-        exit(0);
-}
-
-void exitIfTimeRunOut(unsigned int exit_code) {
-    if (secondsElapsed() > program_duration)
-        exit(exit_code);
+bool isObstacleInFront() {
+    // Check if there is an obstacle within the threshold distance in front of the robot
+    return front_distance < WALL_DISTANCE_THRESHOLD;
 }
