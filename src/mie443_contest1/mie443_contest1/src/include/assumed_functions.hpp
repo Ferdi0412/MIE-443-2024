9557ebbf98c96248c392194d3e88f5b3952764c4
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-02-11 11:06:25 -0500

    Some functions...

---
+++ src/mie443_contest1/mie443_contest1/src/include/assumed_functions.hpp
@@ -0,0 +1,128 @@
+/**
+ * This lays out the format of the functions we discussed in our earlier meeting
+*/
+#ifndef ASSUMED_FUNCTIONS_HPP
+#define ASSUMED_FUNCTIONS_HPP
+
+
+#include "../robot.cpp"
+
+#define REACHED_TARGET 0
+#define WALL_BUMPED 1
+#define WALL_IN_FRONT 2
+
+
+
+/**
+ * getWallAngleFromLaserScan returns the angle the robot must turn to be NORMAL to a wall in the center of it's laser-scan
+ *
+ * @param laser_scan the laser scan vector
+ * @returns an angle in range [-180, 180] if successful, or inf if unsuccessful (ie. no wall in front, or error)
+*/
+float getWallAngleFromLaserScan( Team1::Robot& robot );
+
+
+
+/**
+ * turnRobotBy will turn the robot by a given amount
+ *
+ * @param robot the robot object
+ * @param angle the clockwise-positive angle
+ * @returns REACHED_TARGET if no errors, WALL_BUMPED if bumped a wall
+ * @throws NOTHING -> kill all exceptions
+*/
+int turnRobotBy( Team1::Robot& robot, double angle );
+
+
+
+/**
+ * moveForwardsBy will move the robot forwards
+ *
+ * @param robot           the robot object
+ * @param target_distance the distance to travel forwards
+ * @param wall_distance   the distance from a wall at which it should stop
+ * @returns REACHED_TARGET if target distance travelled, WALL_BUMPED if a wall was bumped, or WALL_IN_FRONT if something in laser scan
+ * @throws NOTHING -> kill all exceptions
+*/
+int moveForwardsBy( Team1::Robot& robot, double target_distance, float wall_distance );
+
+
+
+/**
+ * getRandomValue returns a random value between min_val and max_val
+ *
+ * @param min_val
+ * @param max_val
+ * @returns value between the min_val and max_val
+*/
+float getRandomValue( float min_val, float max_val );
+
+
+
+// Enum for use with followWall
+enum wallDirectionEnum { left, right, any };
+
+/**
+ * followWall makes the robot follow a straight segment of a wall detected in front of the robot
+ *
+ * @param robot the robot object
+ * @param wall_direction the side of the robot on which to keep the wall
+ * @returns .... TBD
+*/
+int followWall( Team1::Robot& robot, wallDirectionEnum wall_direction );
+
+
+
+/**
+ * randomMotion will complete a linear motion in a random direction
+ *
+ * @param robot the robot object
+ * @returns ... TBD
+*/
+int randomMotion( Team1::Robot& robot );
+
+
+
+/**
+ * randomMotion [OVERLOAD] same as randomMotion, but takes a min_angle and max_angle input...
+ *
+ * @param robot the robot object
+ * @param min_angle the minimum acceptable clockwise-positive rotation
+ * @param max_angle the maximum acceptable clockwise-positive rotation
+ * @returns .... SAME as default randomMotion
+*/
+int randomMotion( Team1::Robot& robot, double min_angle, double max_angle );
+
+
+
+/**
+ * rotateAfterBumper will move the robot after it has bumped into a wall (bumper is still triggered)...
+ *
+ * @param robot the robot object
+ * @returns REACHED_TARGET if successful, WALL_BUMPED if bumped into wall when rotating...
+*/
+int rotateAfterBumper( Team1::Robot& robot );
+
+
+
+/**
+ * checkIfFacingCorner will return true if the robot is facing a 90 (or so) degree corner in the maze
+*/
+bool checkIfFacingCorner( Team1::Robot& robot );
+
+
+
+/**
+ * checkIfFacingRoundObject will return true if the robot is facing a round obstacle...
+*/
+bool checkIfFacingRoundObject( Team1::Robot& robot );
+
+
+
+/**
+ * turnAndScanForOpenings will turn the robot to face the biggest opening it detects...
+*/
+int turnAndScanForOpenings( Team1::Robot& robot );
+
+
+#endif
---
+++ src/mie443_contest1/mie443_contest1/src/include/ferd_functions.cpp
@@ -0,0 +1,137 @@
+#ifndef FERDI_FUNCTIONS_CPP
+#define FERDI_FUNCTIONS_CPP
+
+
+#include "assumed_functions.hpp"
+#include "lin_approx.hpp"
+
+#include <vector>
+#include <limits>
+#include <cmath>
+
+#define LINEAR_LIMIT 0.5
+#define ROTATIONAL_VELOCITY 50
+#define LINEAR_VELOCITY 0.2
+
+#ifndef RAD2DEG
+#define RAD2DEG(rad) ((rad) * 180. / M_PI)
+#endif
+
+#ifndef DEG2RAD
+#define DEG2RAD(deg) ((deg) * M_PI / 180.)
+#endif
+
+
+
+
+float getWallAngleFromLaserScan( Team1::Robot& robot ) {
+    lin_approx_t linear_approximation;
+    std::vector<float> laser_ranges;
+    float slope, angle;
+
+    laser_ranges = robot.getRanges();
+
+    linear_approximation = linearApproximation( laser_ranges, laser_ranges.size() * 2 / 5, laser_ranges.size() * 3 / 5 );
+
+    if ( checkApproximationError(linear_approximation) ) return std::numeric_limits<float>::infinity();
+
+    if ( !isStraightLine(linear_approximation, LINEAR_LIMIT) ) return std::numeric_limits<float>::infinity();
+
+    slope = getSlope( linear_approximation );
+
+    // Get normal angle from slope...
+    angle = RAD2DEG(acos( slope ));
+
+    return angle;
+}
+
+
+
+int turnRobotBy( Team1::Robot& robot, double angle ) {
+    try
+        robot.rotateClockwiseBy( ROTATIONAL_VELOCITY, angle );
+    catch (BumperException)
+        return WALL_BUMPED;
+    return REACHED_TARGET;
+}
+
+
+
+int moveForwards( Team1::Robot& robot, double target_distance, float wall_distance ) {
+    double start_x, start_y;
+    std::vector<float> laser_scan;
+
+    // Update positions
+    robot.spinOnce();
+
+    // Store starting positions
+    start_x = robot.getX();
+    start_y = robot.getY();
+
+    // Try to start forwards motion...
+    try {
+        if ( target_distance > 0 )
+            robot.jogForwardsSafe( LINEAR_VELOCITY );
+        else if ( target_distance < 0 )
+            robot.jogForwardsSafe( -LINEAR_VELOCITY );
+        else
+            return REACHED_TARGET;
+    }
+    catch (BumperException)
+        return WALL_BUMPED;
+
+    // During motion... Until target_distance is reached...
+    while ( robot.distanceToPoint( start_x, start_y ) < target_distance ) {
+        robot.spinOnce();
+
+        // Check bumpers
+        if ( robot.getBumperAny() ) {
+            robot.stopMotion();
+            return WALL_BUMPED;
+        }
+
+        // Check distance in laser_scan
+        laser_scan = robot.getRanges();
+
+        // Calculated distance to wall in front
+        if ( getDistanceToWall( laser_scan ) <= wall_distance ) {
+            robot.stopMotion();
+            return WALL_IN_FRONT;
+        }
+    }
+
+    // If no early stop... stop motion and return that target location was reached...
+    robot.stopMotion();
+    return REACHED_TARGET;
+}
+
+
+
+int rotateAfterBumper( Team1::Robot& robot ) {
+    try {
+        // If center bumper pressed... TBD
+        if ( robot.getBumperCenter() ) {
+            // Do something...
+        }
+        // If left bumper is pressed, rotate clockwise
+        else if ( robot.getBumperLeft() ) {
+            robot.moveForwards(-LINEAR_VELOCITY, 0.2);
+            robot.rotateClockwiseBy(ROTATIONAL_VELOCITY, 45);
+        }
+        // If right bumper is pressed, rotate counter-clockwise
+        else if ( robot.getBumperRight() ) {
+            robot.moveForwards(-LINEAR_VELOCITY, 0.2);
+            robot.rotateClockwiseBy(ROTATIONAL_VELOCITY, -45);
+        }
+    }
+    // If any given movement fails, return WALL_BUMPED
+    catch (BumperException)
+        return WALL_BUMPED;
+
+    // Otherwise return REACHED_TARGET when all movements complete
+    return REACHED_TARGET;
+}
+
+
+
+#endif
