732f0ed4e78502edd94c317dcc5ff92d13ee7137
parinj05 <66892409+parinj05@users.noreply.github.com>
2024-02-12 16:59:36 -0500

    Testing parin_functions.cpp

--- src/mie443_contest1/mie443_contest1/src/include/parin_functions.cpp
+++ src/mie443_contest1/mie443_contest1/src/include/parin_functions.cpp
@@ -20,7 +20,7 @@ int avoidObstacles(Team1::Robot& robot, wallDirectionEnum dir);
 int wallFollow( Team1::Robot& robot, wallDirectionEnum wall_direction ) {
     // Parallelize with wall
     int wall_turn = wallParallel(robot);
-    if ( (wall_turn == WALL_NOT_FOUND) || (wall_turn == NO_MOVE) ) return WALL_NOT_FOUND;
+    // if ( (wall_turn == WALL_NOT_FOUND) || (wall_turn == NO_MOVE) ) return WALL_NOT_FOUND;
     robot.spinOnce();
 
     // Obstacle Avoidance Algorithm
@@ -35,42 +35,51 @@ int wallParallel(Team1::Robot& robot) {
 
     const float MAX_DISTANCE = 0.7;
     // Laser scan data
-    // const std::vector<float> laser_ranges = robot.getRanges();
-    // const float right_value = laser_ranges[0];
-    // const float left_value = laser_ranges[laser_ranges.size() - 1];
-
+    const std::vector<float> laser_ranges = robot.getRanges();
+    const float right_value = laser_ranges[0];
+    const float left_value = laser_ranges[laser_ranges.size() - 1];
+    const float middle_value = laser_ranges[laser_ranges.size()/2];
+    float wall_angle = 90 - getWallAngleFromLaserScan(robot);
     // Wall angle data
-    float wall_angle = getWallAngleFromLaserScan(robot);
-    double distance_to_wall = distanceToWall(robot);
+    // float wall_angle = getWallAngleFromLaserScan(robot);
+    // double distance_to_wall = distanceToWall(robot);
 
     // if ( std::isinf(wall_angle) ) {
     //     ROS_ERROR("Wall angle is inf!!!\n");
     //     return NO_MOVE;
     // }
 
-    if ( distance_to_wall >= MAX_DISTANCE) {
-        moveForwardsBy(robot, distance_to_wall - MAX_DISTANCE, MAX_DISTANCE);
-        robot.spinOnce();
-        wall_angle = getWallAngleFromLaserScan(robot);
-    }
+    // if ( middle_value >= MAX_DISTANCE) {
+    //     moveForwardsBy(robot, middle_value - MAX_DISTANCE, MAX_DISTANCE);
+    //     robot.spinOnce();
+    //     wall_angle = getWallAngleFromLaserScan(robot);
+    // }
 
+    std::cout << "Distance to wall: " << middle_value << std::endl;
+    std::cout << "Wall Angle: " << wall_angle << std::endl;
+    
     if ( std::isinf(wall_angle) ) {
         ROS_WARN("Wall angle is inf!!!\n");
         return WALL_NOT_FOUND;
     }
 
-    if (wall_angle < 0){
-        ROS_INFO("Turn CW");
-        turnRobotBy(robot, -wall_angle);
-        return -2; //Turn CW
-    } else if (wall_angle > 0){
-        ROS_INFO("Turn CCW");
-        turnRobotBy(robot, -wall_angle);
-        return -1; //Turn CCW
-    } else if (wall_angle == 0){
-        ROS_INFO("Already PARALLEL");
-        return 0; // Already parallel
-    }
+    if ( middle_value <= MAX_DISTANCE) {
+        if (wall_angle < 0){
+            ROS_INFO("Turn CW");
+            turnRobotBy(robot, -wall_angle);
+            return -2; //Turn CW
+        } else if (wall_angle > 0){
+            ROS_INFO("Turn CCW");
+            turnRobotBy(robot, -wall_angle);
+            return -1; //Turn CCW
+        } else if (wall_angle == 0){
+            ROS_INFO("Already PARALLEL");
+            return 0; // Already parallel
+        } 
+    } 
+
+    return 0;
+    
 
 }
 
@@ -97,7 +106,7 @@ int avoidObstacles(Team1::Robot& robot, wallDirectionEnum dir) {
         if (dir == any){
             if (middle_value > MAX_DISTANCE){
                 ROS_INFO("Move FORWARD");
-                moveForwardsBy(robot,0.2,MAX_DISTANCE);
+                moveForwardsBy(robot, middle_value - MAX_DISTANCE,MAX_DISTANCE);
                 return 0;
             } else if (right_value >= left_value){
                 ROS_INFO("TURN RIGHT");
