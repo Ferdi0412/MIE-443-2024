d78fc35fc94609f78e5718517d8039a47a68bfab
ajeyamadhavarao <ajeyamadhavarao@gmail.com>
2024-02-12 10:02:54 -0500

    copied contest 1

--- src/mie443_contest1/mie443_contest1/src/ajeya_test.cpp
+++ src/mie443_contest1/mie443_contest1/src/ajeya_test.cpp
@@ -5,64 +5,58 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include <queue>
+#include <boost/circular_buffer.hpp>
+
 // ROS imports
 #include <ros/console.h>
 #include "ros/ros.h"
 
-#include <geometry_msgs/Twist.h>
-#include <kobuki_msgs/BumperEvent.h>
-#include <sensor_msgs/LaserScan.h>
-
 // Team1::Robot import
 #include "robot.cpp"
 
-// Temp imports
-#include <cmath>
-#include <iostream>
-#include <vector>
+// Higher level movements
+#include "include/assumed_functions.hpp"
 
+// Ferdi's functions
+#include "include/ferd_functions.cpp"
 
-/**
- * ==============================
- * === FUNCTIONS DECLARATIONS ===
- * ==============================
-*/
+// Parin's functions
+#include "include/parin_functions.cpp"
 
-// trying to get to use this as an interrupt in the main obs avoid function
-class CollisionException : std::exception {};
+// Emma's functions
+#include "include/emma_functions.cpp"
 
+// Ajeya's Functions
+#include "include/ajeya_functions.cpp"
 
+// Linear approximation dependency
+#include "include/lin_approx.cpp"
 
-/**
- * secondsElapsed
- *
- * @returns number of seconds from program_start
-*/
-uint16_t secondsElapsed(void);
+// ========= AUXILLIARY =========
+#define WALL_DISTANCE 0.2
 
-/**
- * printVectorFloats prints each float in a vector
-*/
+typedef unsigned long long time_elapsed_t;
 
-int detectObstacles(Team1::Robot& robot);
-void dodgeObstacles(Team1::Robot& robot, int obstacle);
-void printVectorFloats( const std::vector<float>& the_vector );
-bool avoidObstacles(Team1::Robot& robot);
-void rotateAfterBumper(Team1::Robot& robot);
+time_elapsed_t secondsElapsed(void);
 
-/**
- * =====================
- * === GLOBAL params ===
- * =====================
-*/
+time_elapsed_t getCurrentTime( void );
+
+void storeBumperTimestamp( void ); // Stores the current timestamp in the bumper events queue
+
+void initializeBumperQueue( void );
+
+boost::circular_buffer<time_elapsed_t> bumper_queue(3); // 3 Bumper events...
+
+long long timeSinceFirstBumper( void );
+
 static std::chrono::time_point<std::chrono::system_clock> program_start;
-static const unsigned long long program_duration = 500;
 
+static time_elapsed_t program_duration = 480;
 
+int move_res = 0;
 
-#define SPEED_HIGH 0.1
-// #define ROT_HIGH 0.2
-
+wallDirectionEnum direction = any;
 
 /**
  * ============
@@ -72,92 +66,128 @@ static const unsigned long long program_duration = 500;
  * @param argc number of params used in starting program (ignore but keep)
  * @param argv string params used when starting program  (ignore but keep)
 */
-
-
-
-
-
-// ####################
-//  MAIN LOOP         #
-// ####################
-
 int main ( int argc, char **argv ) {
-    // ROS setup
+    // === SETUP ===
+    ROS_INFO("SETUP...\n");
+
+    initializeBumperQueue();
+
     ros::init(argc, argv, "contest1");
-    ROS_INFO("Starting up...\n");
+
     ros::NodeHandle nh;
-    ros::Rate loop_rate(2);
-    ROS_INFO("Creating Robot");
-
-    // Robot object setup
     Team1::Robot robot( nh, 2);
-    // establishing contact with callback functions and refreshing data input readings
-    robot.spinOnce();
-    // Sleep to ensure is initialized correctly
-    ros::Duration(0.5).sleep(); 
 
-    // GLOBAL params setup
+    // Sleep to ensure is initialized correctly
+    ros::Duration(0.5).sleep();
+    robot.spinOnce();
+
+    // Start timer
     program_start = std::chrono::system_clock::now();
 
-    int final_obstacle_type = 0;
-    int movement_stopped = 0;
+    // Add in to wait on laser_ranges
+    robot.waitOnLaserRanges(); // FERDI -> To implement
 
-
-
-    // ####################
-    //  CONTROL LOOP      #
-    // ####################
-
-    while ( ros::ok() && secondsElapsed() <= program_duration && final_obstacle_type != 69) {
-
-        // ajeya's obstacle avoidance code
-
-        robot.spinOnce();
-        
-        // add try that handles obstacle avoidance using laser scanners
-
-        try {
-            robot.spinOnce();
-            // robot.rotateClockwiseBy(60, 90);
-            // robot.moveForwards(0.2,0.3);
-            final_obstacle_type = detectObstacles(robot);
-            std::cout << "Obstacle Type: " << final_obstacle_type << std::endl;
-            
-            if (final_obstacle_type == 1){
-                robot.moveForwards(0.1,0.1);
-                ROS_INFO("Sailing forward...");
+    // === MAIN ===
+    // loop until program_duration [seconds] is reached
+    while ( ros::ok() && secondsElapsed() <= program_duration ) {
+        robot.spinOnce(); // Update values....
+        /**
+         * === MOVEMENTS FAILED ===
+        */
+        if ( move_res > 0 ) { // If move_res > 0 -> some movement failed/returned early
+            ROS_INFO("Handling movement 'exception'...\n");
+            // Handle cases such as bumper triggered here...
+            // Rotate after bumping into a wall
+            if ( move_res == WALL_BUMPED ) { // EMMA -> Go through and check logic
+                if ( wallInFront( robot ) && distanceToWall( robot ) > 0.1 ) {
+                    move_res = rotateAfterBumper( robot );
+                }
+                else move_res = rotateAfterBumper( robot );
             }
-            else if (final_obstacle_type == 69){
-                // robot.rotateClockwiseBy(60, 90);
-                ROS_INFO("Error 69...");
-            }
-            
-
-        } catch (CollisionException){
-            // bumper exception kills the program as it is being caught before the collision exception
-            ROS_INFO("Obstacle Detected...");
-
-            try {
-
-                dodgeObstacles(robot, final_obstacle_type);
-                ROS_INFO("Dodged...");
-
-            } catch (BumperException) {
-                rotateAfterBumper(robot);
-            }
-
+            // Do nothing if wall scan stopped motion -> allow the wallFollow to work if needed...
+            else if ( move_res == WALL_IN_FRONT ) ;
         }
 
+        /** ALL -> consider this
+         * TBD: Use odometry to track previous movements/positions to prevent stuck in cycle/loop
+        */
 
+        /**
+         * === BUMPED TOO OFTEN ===
+        */
+        if ( timeSinceFirstBumper() < 10 ) { // If too many bumps in the last 10 seconds...
+            ROS_WARN("=== BUMPED TOO OFTEN ===\n");
 
-        
+            // NOTE: If this block just ran, and move_res indicates a wall bummp, you likely didn't do much...
+            // Consider "forceful turn..." -> moves regardless of bumping
+
+            // Implement logic here...
+            move_res = turnRobotBy( robot, 180 );
+            if ( move_res > 0 ) continue; // Go to the MOVEMENTS FAILED section...
+            move_res = randomMotion( robot, -45, 45 );
+            continue;
+        }
+
+        // STARTUP/NORMAL CYCLE
+        if ( wallInFront( robot ) ) {
+            move_res = wallFollow( robot, direction );
+            continue;
+        }
+        else if ( emptyInFront( robot ) ) {
+            moveForwardsBy( robot, 0.2, WALL_DISTANCE );
+            continue;
+        }
+        else if ( checkIfFacingCorner( robot,  WALL_DISTANCE) ) { // V-Shaped corner
+            move_res = turnRobotBy( robot, 45 );
+            continue;
+        }
+        else {
+            move_res = scanForArea( robot );
+
+            // If scanForArea failed
+            if ( move_res > 0 )
+                move_res = randomMotion( robot, -90, 90 );
+            continue;
+        }
+
+        /**
+         * === STARTUP ===
+         * 1. Check for wall   -> if true, wallFollow move in that direction until wall
+         * 1b. Check if open in front ->
+         * 2. Check for corner -> if true, turn 45 degrees and next cycle
+         * 3. scanForArea      -> move in that direction until wall
+         *                     -> if scanForArea issue -> randomMotion -> move in that direction until wall
+        */
+
+        /**
+         * === NORMAL CYCLE ===
+         * SAME AS STARTUP
+        */
+
+        /**
+         * === BUMPED TOO OFTEN ===
+         * - Store vector/fixed sized queue of timestamps
+         * -> if first element too recent, run this...
+         *
+         * 3 bumps -> within 10 seconds (something on ground, or stuck in corner)
+         *
+         * 1. turn 180
+         * 2. small random rotation -> +/- 45 degrees???
+        */
+
+        /**
+         * === BUMP INTO OBJECT BUT NO SCAN ===
+         * - account for object on ground
+        */
+
         robot.sleepOnce();
     }
 
-    robot.stopMotion();
-    ROS_INFO("Stopping robot!\n");
-}
 
+    // === PROGRAM END ===
+    robot.stopMotion();
+    ROS_INFO("ENDED...\n");
+}
 
 
 
@@ -166,270 +196,29 @@ int main ( int argc, char **argv ) {
  * === FUNCTIONS IMPLEMENTATIONS ===
  * =================================
 */
-
-// Function to make the robot detect obstacles hopefully without hitting them
-int detectObstacles(Team1::Robot& robot) {
-    robot.stopMotion();
-
-    const float MIN_DISTANCE = 0.5; // Minimum distance to consider an obstacle
-    const float MAX_DISTANCE = 0.75; // Maximum distance to consider an obstacle
-
-    // the_vector stores all the laserScan readings
-    const std::vector<float> the_vector = robot.getRanges();
-    std::cout << "No. of laser data points: " << the_vector.size() << std::endl;
-
-
-    // // port side scanned value (the_vector[0])
-    // float left_scan_value = the_vector.front();
-    // std::cout << "<<<--- Port Side: " << left_scan_value;
-    // // head on distance is the middle value of the peripheral field of view
-    // int vector_size = the_vector.size()/2;
-    // float middle_scan_value = the_vector[vector_size];
-    // std::cout << "      ^^^ Fore Side: " << middle_scan_value;
-    // // starboard side scanned value (the_vector[-1])
-    // float right_scan_value = the_vector.back();
-    // std::cout << " ^^^      Starboard Side: " << right_scan_value;
-    // std::cout << " --->>>" << std::endl;
-
-    float left_scan_value = std::isnan(the_vector.front()) ? std::numeric_limits<float>::infinity() : the_vector.front();
-    std::cout << "<<<--- Port Side: " << left_scan_value;
-
-    // Head-on distance is the middle value of the peripheral field of view
-    int vector_size = the_vector.size();
-    float middle_scan_value = (vector_size > 0 && !std::isnan(the_vector[vector_size / 2])) ? the_vector[vector_size / 2] : std::numeric_limits<float>::infinity();
-    std::cout << "      ^^^ Fore Side: " << middle_scan_value;
-
-    // Starboard side scanned value
-    float right_scan_value = std::isnan(the_vector.back()) ? std::numeric_limits<float>::infinity() : the_vector.back();
-    std::cout << " ^^^      Starboard Side: " << right_scan_value;
-
-    std::cout << " --->>>" << std::endl;
-    
-    
-
-    // check if laser is sending legitimate values if not, close function
-    if (!the_vector.empty()) {
-        int obstacle_type = 0;
-        std::cout << "Obstacle type before loop: " << obstacle_type << std::endl;
-
-
-        if (left_scan_value <= MIN_DISTANCE && right_scan_value >= left_scan_value && middle_scan_value >= left_scan_value){
-            // Robot faces a wall on the left
-            obstacle_type = 111;
-            std::cout << "Obstacle type after loop: " << obstacle_type << std::endl;
-
-            throw CollisionException();
-            return obstacle_type;
-        }
-        else if (right_scan_value <= MIN_DISTANCE && right_scan_value <= middle_scan_value && middle_scan_value <= left_scan_value){
-            // Robot faces a wall on the right
-            obstacle_type = 333;
-            std::cout << "Obstacle type after loop: " << obstacle_type << std::endl;
-
-            throw CollisionException();
-            return obstacle_type;
-
-        }
-        else if (middle_scan_value <= MIN_DISTANCE && right_scan_value >= middle_scan_value && middle_scan_value <= left_scan_value){
-            // Robot faces a wall head on
-            obstacle_type = 222;
-            std::cout << "Obstacle type after loop: " << obstacle_type << std::endl;
-
-            throw CollisionException();
-            return obstacle_type;
-
-        }
-
-        else if (left_scan_value <= MIN_DISTANCE){
-            obstacle_type = 11;
-            std::cout << "Obstacle type after loop: " << obstacle_type << std::endl;
-
-            throw CollisionException();
-            return obstacle_type;
-
-        }
-        else if (right_scan_value <= MIN_DISTANCE){
-            obstacle_type = 33;
-            std::cout << "Obstacle type after loop: " << obstacle_type << std::endl;
-
-            throw CollisionException();
-            return obstacle_type;
-
-        }
-        else if (middle_scan_value <= MIN_DISTANCE){
-            obstacle_type = 22;
-            std::cout << "Obstacle type after loop: " << obstacle_type << std::endl;
-
-            throw CollisionException();
-            return obstacle_type;
-
-        }
-        else{
-            // no obstacle
-            obstacle_type = 1;
-            std::cout << "Obstacle type after loop: " << obstacle_type << std::endl;
-            return obstacle_type;
-            
-
-        }
-    } else {
-        std::cout << "Vector is empty!" << std::endl;
-        return 69;
-    }
-}
-
-// Function to make the robot detect obstacles hopefully without hitting them
-void dodgeObstacles(Team1::Robot& robot, int obstacle) {
-    const float MIN_DISTANCE = 0.2; // Minimum distance to consider an obstacle
-    const float MAX_DISTANCE = 0.3; // Maximum distance to consider an obstacle
-
-    // the_vector stores all the laserScan readings
-    const std::vector<float> the_vector = robot.getRanges();
-    std::cout << "No. of laser data points: " << the_vector.size() << std::endl;
-
-
-    // // port side scanned value (the_vector[0])
-    // float left_scan_value = the_vector.front();
-    // std::cout << "<<<--- Port Side: " << left_scan_value;
-    // // head on distance is the middle value of the peripheral field of view
-    // float middle_scan_value = the_vector[the_vector.size() / 2];
-    // std::cout << "      ^^^ Fore Side: " << middle_scan_value;
-    // // starboard side scanned value (the_vector[-1])
-    // float right_scan_value = the_vector.back();
-    // std::cout << " ^^^      Starboard Side: " << right_scan_value;
-    // std::cout << " --->>>" << std::endl;
-
-    float left_scan_value = std::isnan(the_vector.front()) ? std::numeric_limits<float>::infinity() : the_vector.front();
-    std::cout << "<<<--- Port Side: " << left_scan_value;
-
-    // Head-on distance is the middle value of the peripheral field of view
-    int vector_size = the_vector.size();
-    float middle_scan_value = (vector_size > 0 && !std::isnan(the_vector[vector_size / 2])) ? the_vector[vector_size / 2] : std::numeric_limits<float>::infinity();
-    std::cout << "      ^^^ Fore Side: " << middle_scan_value;
-
-    // Starboard side scanned value
-    float right_scan_value = std::isnan(the_vector.back()) ? std::numeric_limits<float>::infinity() : the_vector.back();
-    std::cout << " ^^^      Starboard Side: " << right_scan_value;
-
-    std::cout << " --->>>" << std::endl;
-
-    bool wall_on_left,wall_on_right, wall_head_on;
-    bool obstacle_on_left, obstacle_on_right, obstacle_head_on;
-
-    // check if laser is sending legitimate values if not, close function
-    if (!the_vector.empty()) {
-
-        // Part 1: Wall detection
-
-        if (obstacle == 111){
-            // Robot faces a wall on the left
-            // change orientation to head on to the wall and then rotate 90
-            // dont do this if walls on all three sides
-
-            bool wall_on_left = true;
-            ROS_INFO("Port Side Wall Detected...");
-
-        }
-        if (obstacle == 333){
-            // Robot faces a wall on the right
-
-            bool wall_on_right = true;
-            ROS_INFO("Starboard Side Wall Detected...");
-
-        }
-        if (obstacle == 222){
-            // Robot faces a wall head on
-
-            bool wall_head_on = true;
-            ROS_INFO("Head on Wall Detected...");
-
-        }
-
-        // Part 2: Object Detection
-
-        if (obstacle == 11){
-            bool obstacle_on_left = true;
-        }
-        if (obstacle == 33){
-            bool obstacle_on_right = true;
-        }
-        if (obstacle == 22){
-            bool obstacle_head_on = true;
-        }
-
-        // Part 3: Correction 
-        // ik part 1 and part 2 can be removed but i will do that later
-
-            // obstacles
-        if (obstacle_on_left){
-            robot.stopMotion();
-            robot.rotateClockwiseBy(50,45);
-            ROS_INFO("Rotating CW...");
-            // return 0;
-        }else if (obstacle_on_right){
-            robot.stopMotion();
-            robot.rotateClockwiseBy(50,-45);
-            ROS_INFO("Rotating CCW...");
-            // return 0;
-        }else if (obstacle_head_on){
-            robot.stopMotion();
-            robot.rotateClockwiseBy(50,180);
-            ROS_INFO("Rotating 180 CW...");
-            // return 0;
-        }
-
-
-            // walls (NOT DONE YET)
-        if (wall_on_left){
-            robot.stopMotion();
-            robot.rotateClockwiseBy(50,45);
-            ROS_INFO("Rotating CW...");
-            // return 0;
-        }else if (wall_on_right){
-            robot.stopMotion();
-            robot.rotateClockwiseBy(50,-45);
-            ROS_INFO("Rotating CCW...");
-            // return 0;
-        }else if (wall_head_on){
-            robot.stopMotion();
-            robot.rotateClockwiseBy(50,180);
-            ROS_INFO("Rotating 180 CW...");
-            // return 0;
-        }
-
-        // add inverse trig parallelize code
-
-    } else {
-        std::cout << "Vector is empty!" << std::endl;
-        // throw BumperException();
-        // return 1;
-    }
-}
-
-uint16_t secondsElapsed( void ) {
+// ========= EDIT THE FOLLOWING DEFINITION BELOW =========
+time_elapsed_t secondsElapsed( void ) {
     return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - program_start).count();
 }
 
-void printVectorFloats( const std::vector<float>& the_vector ) {
-    // std::cout << the_vector.size();
-    for ( unsigned int i = 0; i < the_vector.size(); i++ )
-        std::cout << the_vector[i] << "; ";
-    //for ( const float& val : the_vector )
-    //    std::cout << val << "; ";
-    std::cout << "\n";
+
+time_elapsed_t getCurrentTime( void ) {
+    return secondsElapsed();
 }
 
-void rotateAfterBumper(Team1::Robot& robot){
-            if (robot.getBumperRight() == true){
-                robot.moveForwards(-0.25,0.2);
-                robot.rotateClockwiseBy(60, -45);
-            }
-            else if (robot.getBumperLeft() == true){
-                robot.moveForwards(-0.25,0.2);
-                robot.rotateClockwiseBy(60, 45);
-            }
-            else {
-            robot.moveForwards(-0.25,0.2);
+
+void storeBumperTimestamp( void ) {
+    bumper_queue.push_back( getCurrentTime() );
 }
-    robot.spinOnce();
-}   
+
+
+void initializeBumperQueue( void ) {
+    while ( bumper_queue.size() > 0 )
+        bumper_queue.pop_front();
+}
+
+long long timeSinceFirstBumper( void ) {
+    if ( bumper_queue.size() == bumper_queue.capacity() )
+        return (getCurrentTime() - bumper_queue.front());
+    return -1;
+}
