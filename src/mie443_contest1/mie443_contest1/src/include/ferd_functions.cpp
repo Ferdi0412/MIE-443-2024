50a4a6cd51cd5f4a3708dcdd63eada686f53d40f
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-02-13 09:03:21 -0500

    Testing wall functions

--- src/mie443_contest1/mie443_contest1/src/include/ferd_functions.cpp
+++ src/mie443_contest1/mie443_contest1/src/include/ferd_functions.cpp
@@ -31,9 +31,13 @@
 #endif
 
 
+#define SCAN_START 4
+#define SCAN_END   5
+
+
 bool wallInFront( Team1::Robot& robot ) {
     lin_approx_t linear_approximation;
-    linear_approximation = linearApproximation( robot.getRanges(), robot.getRanges().size() * (NUM_SCAN_SEGM / 2) / NUM_SCAN_SEGM, robot.getRanges().size() * (NUM_SCAN_SEGM / 2 + 1) / NUM_SCAN_SEGM);
+    linear_approximation = linearApproximation( robot.getRanges(), robot.getRanges().size() * SCAN_START / NUM_SCAN_SEGM, robot.getRanges().size() * SCAN_END / NUM_SCAN_SEGM);
     if ( checkApproximationError( linear_approximation) ) return false;
     ROS_INFO("=== wallInFront ---> MSE: %.4f\n", getMeanSquaredError(linear_approximation));
     return isStraightLine(linear_approximation, 0.00005);
@@ -48,7 +52,7 @@ float getWallAngleFromLaserScan( Team1::Robot& robot ) {
 
     laser_ranges = robot.getRanges();
 
-    linear_approximation = linearApproximation( robot.getRanges(), robot.getRanges().size() * (NUM_SCAN_SEGM / 2) / NUM_SCAN_SEGM, robot.getRanges().size() * (NUM_SCAN_SEGM / 2 + 1) / NUM_SCAN_SEGM);
+    linear_approximation = linearApproximation( robot.getRanges(), robot.getRanges().size() * SCAN_START / NUM_SCAN_SEGM, robot.getRanges().size() * SCAN_END / NUM_SCAN_SEGM);
 
     if ( checkApproximationError(linear_approximation) ) return std::numeric_limits<float>::infinity();
 
