13127f97e899a7f172fc8321a81bb17a5c445759
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-02-15 10:29:16 -0500

    Removed unused file...

--- src/mie443_contest1/mie443_contest1/src/include/random_movememnt.hpp
+++
@@ -1,9 +0,0 @@
-#include "../robot.cpp"
-
-/**
- * moveUntilBump will move the robot forwards in current orientation
- * until it bumps into a wall
- * 
- * @param robot is the robot object to control
-*/
-void moveUntilBump( Team1::Robot robot );
