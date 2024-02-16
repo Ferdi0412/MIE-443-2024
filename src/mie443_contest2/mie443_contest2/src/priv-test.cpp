4b6ba146ac47705c83d6ef253d1e9afaedff99cb
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-02-16 16:56:30 -0500

    Added private test file for contest 2

---
+++ src/mie443_contest2/mie443_contest2/src/priv-test.cpp
@@ -0,0 +1,12 @@
+/**
+ * This file is intended for private testing. It will NOT be commited to GitHub.
+ * Any functions used here, that are intended to be used elsewhere, should be in a seperate file.
+ *
+ * NOTE: If you want to stop the code, and display that an error occured, you have 2 options:
+ * 1. `return` from the main function. A return of 0 indicates successful completion, any other value indicates an error occured.
+ * 2. `exit(0)` from anywhere. This will kill main, and is the same as a line of `return 0` in main.
+*/
+
+int main(int argc, char** argv) {
+
+}
