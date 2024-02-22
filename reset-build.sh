fb9dac01cd88abaaec77d0cc7c61836998ec7541
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-02-22 16:45:43 -0500

    Created reset-build file

---
+++ reset-build.sh
@@ -0,0 +1,12 @@
+base_dir=$(dirname "$(readlink -f "$0")")
+cd base_dir           # Go to reset-build.sh file's directory
+
+
+sudo rm -rf build     # Remove all build files created by catkin_make
+sudo rm -rf devel     # Remove all devel files created by catkin_make
+
+
+catkin_make           # Re-build projecct
+
+
+source devel/setup.sh # re-source
