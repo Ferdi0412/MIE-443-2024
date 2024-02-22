b7f6c37402fc2cb5ea942148f05014be8cc9a044
Ferdinand Tonby-Strandborg <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-02-22 15:13:59 -0500

    Updated private test file for contest 2

--- src/mie443_contest2/mie443_contest2/src/priv-test.cpp
+++ src/mie443_contest2/mie443_contest2/src/priv-test.cpp
@@ -7,6 +7,73 @@
  * 2. `exit(0)` from anywhere. This will kill main, and is the same as a line of `return 0` in main.
 */
 
-int main(int argc, char** argv) {
+/**
+ * ===================
+ * === DEFINITIONS ===
+*/
+#define CONTEST_TIME 300
 
+/**
+ * ===============
+ * === IMPORTS ===
+*/
+// The following files are found in "../include/" directory
+#include <boxes.h>
+#include <navigation.h>
+#include <robot_pose.h>
+#include <imagePipeline.h>
+#include <auxilliary.h>
+
+// The following is a standard "system" import
+#include <chrono>
+
+
+
+/**
+ * ============
+ * === MAIN ===
+*/
+int main(int argc, char** argv) {
+    /**
+     * === ROS SETUP ===
+    */
+    // Setup ROS.
+    ros::init(argc, argv, "contest2");
+    ros::NodeHandle n;
+    // Robot pose object + subscriber.
+    RobotPose robotPose(0,0,0);
+    ros::Subscriber amclSub = n.subscribe("/amcl_pose", 1, &RobotPose::poseCallback, &robotPose);
+
+    // Initialize box coordinates and templates
+    Boxes boxes;
+    if(!boxes.load_coords() || !boxes.load_templates()) {
+        std::cout << "ERROR: could not load coords or templates" << std::endl;
+        return -1;
+    }
+    for(int i = 0; i < boxes.coords.size(); ++i) {
+        std::cout << "Box coordinates: " << std::endl;
+        std::cout << i << " x: " << boxes.coords[i][0] << " y: " << boxes.coords[i][1] << " z: "
+                  << boxes.coords[i][2] << std::endl;
+    }
+
+    // Initialize image objectand subscriber.
+    ImagePipeline imagePipeline(n);
+
+    // contest count down timer
+    mainTimerStart();
+
+
+    /**
+     * === MAIN BODY ===
+    */
+    // Execute strategy.
+    while(ros::ok() && mainTimerSecondsElapsed() <= CONTEST_TIME) {
+        ros::spinOnce();
+        /***YOUR CODE HERE***/
+        // Use: boxes.coords
+        // Use: robotPose.x, robotPose.y, robotPose.phi
+        imagePipeline.getTemplateID(boxes);
+        ros::Duration(0.01).sleep();
+    }
+    return 0;
 }
