6ccbec3398829098576addc1f1949a9b867e8f95
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-02-24 00:04:37 -0500

    Updated defaul box path to be gazebo_coords.xml

--- src/mie443_contest2/mie443_contest2/src/boxes.cpp
+++ src/mie443_contest2/mie443_contest2/src/boxes.cpp
@@ -2,11 +2,11 @@
 #include <boxes.h>
 
 bool Boxes::load_coords( ) {
-    return Boxes::load_coords( "coords.xml" );
+    return Boxes::load_coords( "gazebo_coords.xml" );
 }
 
 bool Boxes::load_coords( std::string file_name ) {
-    std::string filePath = ros::package::getPath("mie443_contest2") + 
+    std::string filePath = ros::package::getPath("mie443_contest2") +
                            std::string("/boxes_database/") + file_name;
     cv::FileStorage fs(filePath, cv::FileStorage::READ);
     if(fs.isOpened()) {
@@ -18,8 +18,8 @@ bool Boxes::load_coords( std::string file_name ) {
         for(int i = 0; i < 5; ++i) {
             node = fs[coords_xml[i]];
             if(node.type() != cv::FileNode::SEQ) {
-                std::cout << "XML ERROR: Data in " << coords_xml[i] 
-                          << " is improperly formatted - check input.xml" << std::endl; 
+                std::cout << "XML ERROR: Data in " << coords_xml[i]
+                          << " is improperly formatted - check input.xml" << std::endl;
             } else {
                 it = node.begin();
                 end = node.end();
@@ -30,13 +30,13 @@ bool Boxes::load_coords( std::string file_name ) {
                 if(coordVec.size() == 3) {
                     coords.push_back(coordVec);
                 } else {
-                    std::cout << "XML ERROR: Data in " << coords_xml[i] 
-                              << " is improperly formatted - check input.xml" << std::endl; 
+                    std::cout << "XML ERROR: Data in " << coords_xml[i]
+                              << " is improperly formatted - check input.xml" << std::endl;
                 }
             }
         }
         if(coords.size() == 0) {
-            std::cout << "XML ERROR: Coordinate data is improperly formatted - check input.xml" 
+            std::cout << "XML ERROR: Coordinate data is improperly formatted - check input.xml"
                   << std::endl;
             return false;
         }
@@ -48,14 +48,14 @@ bool Boxes::load_coords( std::string file_name ) {
 }
 
 bool Boxes::load_templates() {
-    std::string filePath = ros::package::getPath("mie443_contest2") + 
+    std::string filePath = ros::package::getPath("mie443_contest2") +
                            std::string("/boxes_database/templates.xml");
     cv::FileStorage fs(filePath, cv::FileStorage::READ);
     if(fs.isOpened()) {
         cv::FileNode node = fs["templates"];;
         cv::FileNodeIterator it, end;
-        if(!(node.type() == cv::FileNode::SEQ || node.type() == cv::FileNode::STRING)) { 
-            std::cout << "XML ERROR: Image data is improperly formatted in " << filePath 
+        if(!(node.type() == cv::FileNode::SEQ || node.type() == cv::FileNode::STRING)) {
+            std::cout << "XML ERROR: Image data is improperly formatted in " << filePath
                       << std::endl;
             return false;
         }
@@ -63,11 +63,11 @@ bool Boxes::load_templates() {
         end = node.end();
         std::string imagepath;
         for(; it != end; ++it){
-            imagepath = ros::package::getPath("mie443_contest2") + 
-                        std::string("/boxes_database/") + 
+            imagepath = ros::package::getPath("mie443_contest2") +
+                        std::string("/boxes_database/") +
                         std::string(*it);
             templates.push_back(cv::imread(imagepath, CV_LOAD_IMAGE_GRAYSCALE));
-        }        
+        }
     } else {
         std::cout << "XML ERROR: Could not open " << filePath << std::endl;
         return false;
