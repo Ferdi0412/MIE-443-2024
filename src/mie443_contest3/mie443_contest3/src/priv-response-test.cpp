f676bf91ab348bff5f073cf586bcb9e9acecbbaa
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-04-01 00:05:17 -0400

    Added wait for soundclient to be ready

--- src/mie443_contest3/mie443_contest3/src/priv-response-test.cpp
+++ src/mie443_contest3/mie443_contest3/src/priv-response-test.cpp
@@ -10,15 +10,16 @@ int main ( int argc, char **argv ) {
     // Setup misc. stuff
     ros::init(argc, argv, "response_test");
     ros::NodeHandle nh;
-    
+
     // Setup sound_play stuff
     sound_play::SoundClient sc;
     std::string path_to_sounds = ros::package::getPath("mie443_contest3") + "/sounds/";
 
     /* Add any more setup stuff here... */
 
-    // Wait 2 seconds to (hopefully) ensure that subscriptions are working...
-    ros::Duration(2).sleep();
+    // Wait to (hopefully) ensure that soundclient subscription is working...
+    while ( !sc.isSoundPlayReady() )
+        ros::Duration(0.3).sleep();
 
     /**
      * ============
