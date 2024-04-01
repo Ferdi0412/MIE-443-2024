09276902208edcd0609738837de93d38317189ba
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-04-01 15:03:34 -0400

    Fixed timer initialization

--- src/mie443_contest3/mie443_contest3/src/robot_control/program_timer.cpp
+++ src/mie443_contest3/mie443_contest3/src/robot_control/program_timer.cpp
@@ -7,7 +7,7 @@
 typedef std::chrono::time_point<std::chrono::system_clock> _sys_time_t;
 
 static _sys_time_t start_time;
-static bool        is_started;
+static bool        is_started = false;
 
 inline _sys_time_t get_time() {
     return std::chrono::system_clock::now();
@@ -18,8 +18,10 @@ inline long seconds_between( _sys_time_t begin_time, _sys_time_t end_time ) {
 }
 
 long seconds_elapsed( ) {
-    if ( !is_started )
+    if ( !is_started ) {
         start_time = get_time();
+        is_started = true;
+    }
 
     return seconds_between( start_time, get_time() );
 }
