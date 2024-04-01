b89de9b255bfd041f3480b4dc5706c26c5c564d4
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-04-01 03:38:21 -0400

    Template for ImageHandler class - simple wrapper for displaying images

---
+++ src/mie443_contest3/mie443_contest3/src/image_handler/basic_client.cpp
@@ -0,0 +1,27 @@
+#ifndef BASIC_IMAGE_CLIENT_CPP
+#define BASIC_IMAGE_CLIENT_CPP
+
+#include <string>
+
+#include <ros/ros.h>
+
+class ImageHandler {
+    public:
+        void display( std::string jpg_png_filename ) {
+
+        }
+
+        void play ( std::string mp4_filename ) {
+
+        }
+
+        ImageHandler( ros::NodeHandle node_handler ) {
+
+        }
+
+    private:
+
+}
+
+
+#endif // ~ BASIC_IMAGE_CLIENT_CPP
