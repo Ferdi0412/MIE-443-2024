da4a92450246883c2ca11b2186a7710a3bff2a52
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-04-03 02:43:58 -0400

    Added support for IP video streams

--- src/mie443_contest3/mie443_contest3/src/my_publisher_video.cpp
+++ src/mie443_contest3/mie443_contest3/src/my_publisher_video.cpp
@@ -14,13 +14,23 @@ int main(int argc, char** argv)
   image_transport::ImageTransport it(nh);
   image_transport::Publisher pub = it.advertise("camera/image", 1);
 
+  // Initialize video "stream" object
+  cv::VideoCapture cap;
+
   // Convert the passed as command line parameter index for the video device to an integer
   std::istringstream video_sourceCmd(argv[1]);
-  int video_source;
-  // Check if it is indeed a number
-  if(!(video_sourceCmd >> video_source)) return 1;
+  int                video_source; // Used if local camera
+  std::string        video_url = argv[1];    // Used if IP-addressable stream
 
-  cv::VideoCapture cap(video_source);
+  // Check if it is indeed a number - if so open corresponding camera
+  if ( (video_sourceCmd >> video_source) ) {
+    cap.open(video_source);
+  }
+  // Otherwise, assume it is a file or IP address
+  else {
+    cap.open(video_url);
+  }
+
   // Check if video device can be opened with the given index
   if(!cap.isOpened()) return 1;
   cv::Mat frame;
