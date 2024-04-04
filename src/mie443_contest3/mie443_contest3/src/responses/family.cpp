2e7a028f92bfecc1cedfc77305426acec8fe6b3d
Ferdi <ferdinand.tonby.strandborg@gmail.com>
2024-04-04 12:17:53 -0400

    Family responses - for face/image detection

---
+++ src/mie443_contest3/mie443_contest3/src/responses/family.cpp
@@ -0,0 +1,20 @@
+/**
+ * When path is blocked, can be FRUSTRATED or ANGRY
+*/
+#include "responses.h"
+
+/**
+ * frustrated_move_backwards - plays a short sound then moves backwards in hopes of getting past blockage - short wait at back
+*/
+
+
+void happy_family( SoundPlayer& sound_player, ImageHandler& image_handler ) {
+    long long start_time = seconds_elapsed();
+    image_handler.display("family.png");
+    sound_player.play("family.wav");
+    while ( (seconds_elapsed() - start_time < 10) /*&& face_detector.isFaceDetected()*/ ){
+        ros::Duration(0.5).sleep();
+    }
+
+}
+
