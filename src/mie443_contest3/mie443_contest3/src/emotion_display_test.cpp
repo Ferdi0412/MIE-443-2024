278b2f34667f418deda103dddef90999c23ab067
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-04-10 20:56:24 -0400

    Testing emotions... again...

---
+++ src/mie443_contest3/mie443_contest3/src/emotion_display_test.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+#include <header.h>
+
+#include "robot_control/robot_control.h"
+#include "image_handler/basic_client.cpp"
+#include "sound_play/basic_client.cpp"
+#include "responses/responses.h"
+
+int main ( int argc, char ** argv ) {
+    ros::init( argc, argv, "emotion_test" );
+    ros::NodeHandle nh;
+    ros::Rate loop_rate(0.5);
+
+    SoundPlayer  sound_player;
+    ImageHandler image_handler(nh);
+
+    initialize_all( nh );
+
+    while ( 1 ) {
+        std::cout << "PLEASE INPUT A NUMBER FROM 0 TO 5 OR h FOR HELP:\n";
+        std::string user_input;
+        user_input << std::cin;
+
+        switch( user_input ) {
+            case "h":
+                std::cout << "Options:\n0: neutral\n1: confusion\n2: saddness\n3: happy\n4: scared\n 5: discontent!";
+                break;
+
+            case "0":
+                std::cout << "Neutral!\n";
+                display_neutral( sound_player, image_handler );
+                break;
+
+            case "1":
+                std::cout << "Confusion!\n";
+                display_confusion( sound_player, image_handler );
+                break;
+
+            case "2":
+                std::cout << "Sadness!\n";
+                display_sadness( sound_player, image_handler );
+                break;
+
+            case "3":
+                std::cout << "Family!\n";
+                happy_family( sound_player, image_handler );
+                break;
+
+            case "4":
+                std::cout << "Scared!\n";
+                display_scaredness( sound_player, image_handler );
+                break;
+
+            case "5":
+                std::cout << "Discontent!\n";
+                display_disontent( sound_player, image_handler );
+                break;
+
+            default:
+                std::cout << "Could not recognize input! Please try again...\n";
+                break;
+        }
+
+        loop_rate.sleep();
+    }
+}
