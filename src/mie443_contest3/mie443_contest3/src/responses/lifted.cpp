18c355550143507e52e8915b9af60eaed493ab6e
Ferdi0412 <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-04-11 02:16:51 -0400

    Corrected filename

--- src/mie443_contest3/mie443_contest3/src/responses/lifted.cpp
+++ src/mie443_contest3/mie443_contest3/src/responses/lifted.cpp
@@ -8,7 +8,7 @@
  * display_surprised - When being lifted off the ground
 */
 void display_surprised( SoundPlayer& sound_player, ImageHandler& image_handler ) {
-    image_handler.display("surprised.png");
+    image_handler.display("surprised.jpg");
     sound_player.play("surprised2.wav");
     ros::Duration(0.8);
 }
