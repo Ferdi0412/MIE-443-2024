d05f9f790b239970d1daab09d5d2ca5d9c45c659
Ferdinand Tonby-Strandborg <ferdinand.tonby.strandborg@mail.utoronto.ca>
2024-04-10 21:32:00 -0400

    Working basic emotion_display_test

--- src/mie443_contest3/mie443_contest3/src/emotion_display_test.cpp
+++ src/mie443_contest3/mie443_contest3/src/emotion_display_test.cpp
@@ -21,46 +21,50 @@ int main ( int argc, char ** argv ) {
     while ( 1 ) {
         std::cout << "PLEASE INPUT A NUMBER FROM 0 TO 5 OR h FOR HELP:\n";
         std::string user_input;
-        user_input << std::cin;
+        if ( std::cin.eof() )
+            break;
+        std::cin >> user_input;
 
-        switch( user_input ) {
-            case "h":
-                std::cout << "Options:\n0: neutral\n1: confusion\n2: saddness\n3: happy\n4: scared\n 5: discontent!";
-                break;
+        if ( !user_input.empty() ) {
+            switch( user_input[0] ) {
+                case 'h':
+                    std::cout << "Options:\n0: neutral\n1: confusion\n2: saddness\n3: happy\n4: scared\n 5: discontent!";
+                    break;
 
-            case "0":
-                std::cout << "Neutral!\n";
-                display_neutral( sound_player, image_handler );
-                break;
+                case '0':
+                    std::cout << "Neutral!\n";
+                    display_neutral( sound_player, image_handler );
+                    break;
 
-            case "1":
-                std::cout << "Confusion!\n";
-                display_confusion( sound_player, image_handler );
-                break;
+                case '1':
+                    std::cout << "Confusion!\n";
+                    display_confusion( sound_player, image_handler );
+                    break;
 
-            case "2":
-                std::cout << "Sadness!\n";
-                display_sadness( sound_player, image_handler );
-                break;
+                case '2':
+                    std::cout << "Sadness!\n";
+                    display_sadness( sound_player, image_handler );
+                    break;
 
-            case "3":
-                std::cout << "Family!\n";
-                happy_family( sound_player, image_handler );
-                break;
+                case '3':
+                    std::cout << "Family!\n";
+                    happy_family( sound_player, image_handler );
+                    break;
 
-            case "4":
-                std::cout << "Scared!\n";
-                display_scaredness( sound_player, image_handler );
-                break;
+                case '4':
+                    std::cout << "Scared!\n";
+                    display_scaredness( sound_player, image_handler );
+                    break;
 
-            case "5":
-                std::cout << "Discontent!\n";
-                display_disontent( sound_player, image_handler );
-                break;
+                case '5':
+                    std::cout << "Discontent!\n";
+                    display_discontent( sound_player, image_handler );
+                    break;
 
-            default:
-                std::cout << "Could not recognize input! Please try again...\n";
-                break;
+                default:
+                    std::cout << "Could not recognize input! Please try again...\n";
+                    break;
+            }
         }
 
         loop_rate.sleep();
